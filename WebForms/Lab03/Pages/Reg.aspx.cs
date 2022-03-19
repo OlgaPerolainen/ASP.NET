4fdf0067a66ffac5fc1a64d0788579c7b476f9ef
OlgaPerolainen <95132545+OlgaPerolainen@users.noreply.github.com>
2022-03-19 13:23:33 +0300

    Add files via upload

---
+++ WebForms/Lab03/Pages/Reg.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RSVP
+{
+    public partial class Reg : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Проверяем,  загружается ли форма в ответ на обратный запрос клиента
+            if (IsPostBack)
+            {
+                // Создаем экземпляр объекта GuestResponse с полученными от элементов формы данными
+                GuestResponse rsvp = new GuestResponse(txtBoxName.Text, txtBoxEmail.Text, txtBoxPhone.Text, CheckBoxYN.Checked);
+                
+				// Помещаем созданный экземпляр в хранилище
+                ResponseRepository.GetRepository().AddResponse(rsvp);
+
+                // Проверяем, будет ли пользователь выступать с докладом
+                // Предоставляем ответ в зависимости от этого
+                if (rsvp.WillAttend.HasValue && rsvp.WillAttend.Value)
+                {
+                    Response.Redirect(@"..\Responses.Html\seeyouthere.html");
+                }
+                else
+                {
+                    Response.Redirect(@"..\Responses.Html\sorryyoucantperform.html");
+                }
+            }
+        }
+    }
+}
---
+++ WebForms/Lab03/ex2.SeparateCode/Summary.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace RSVP
+{
+    public partial class Summary : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public string GetNonPerfomersHtml()
+        {
+            StringBuilder html = new StringBuilder();
+            var nonPerformers = ResponseRepository.GetRepository().GetAllResponses().Where(r => !r.WillAttend.Value);
+
+            foreach (var rsvp in nonPerformers)
+            {
+                html.Append(String.Format("<tr><td>{0}</td><td>{1}</td><td>{1}</td>", rsvp.Name, rsvp.Email, rsvp.Phone));
+            }
+            return html.ToString();
+        }
+    }
+}
